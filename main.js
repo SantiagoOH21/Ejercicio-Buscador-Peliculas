3dd424dcb8f0e09ccb99c064e9d434519d931f34
SantiagoOH21 <soh0621@gmail.com>
2025-04-22 18:42:41 +0200

    movie genre

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const API_URL = "https://api.themoviedb.org/3/";
-const APIKey = "api_key=29a62049dc373ada0ffcf6191b950e4f";
-const movieURL = "movie/";
+const APIKey = "29a62049dc373ada0ffcf6191b950e4f";
 
 const moviesContainer = document.querySelector("#movies");
 const formSearch = document.getElementById("form");
@@ -9,48 +8,60 @@ const searchInput = document.getElementById("searchId");
 const imageBaseUrl = "https://image.tmdb.org/t/p/";
 const posterSize = "original";
 
+const saveMovieGenres = async () => {
+  try {
+    const res = await axios.get(
+      `${API_URL}genre/movie/list?language=es&api_key=${APIKey}`
+    );
+    const movieGenres = res.data.genres;
+    console.log(movieGenres);
+    return movieGenres;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const changeMovieGenres = (movieGenres, genreIds) => {
+  const matchingGenres = movieGenres.filter((movieGenre) =>
+    genreIds.includes(movieGenre.id)
+  );
+  const genreNames = matchingGenres.map((movieGenre) => movieGenre.name);
+  return genreNames.join(", ");
+};
+
+const genreIds = [18, 28, 53];
+
+const obtainGenre = async () => {
+  const movieGenres = await saveMovieGenres();
+  const genresLog = changeMovieGenres(movieGenres, genreIds);
+  console.log("genresLog:", genresLog);
+};
+
+obtainGenre();
+
 const showMovies = (movies) => {
   moviesContainer.innerHTML = "";
   movies.forEach((movie) => {
     const imageUrl = `${imageBaseUrl}${posterSize}${movie.poster_path}`;
     moviesContainer.innerHTML += `
-      <div class="card col-lg-3 col-xs-12 col-md-6">
+    <div class="card col-lg-3 col-xs-12 col-md-6">
        <img src="${imageUrl}" alt="${movie.title}">
        <div class="card-body">
-        <h3 class="card-header">${movie.title}</h3>
-        <h5 class="card-title">${movie.genre_ids}</h5>
-        <p>${movie.overview}</p>
+       <h3 class="card-header">${movie.title}</h3>
+        <h5 class="card-title">Género: ${movie.genre_ids}</h5>
+        <p>SINOPSIS <br>${movie.overview}</p>
        </div>
       </div>
       `;
   });
 };
 
-// const options = {
-//   method: "GET",
-//   url: "https://api.themoviedb.org/3/search/movie",
-//   params: {
-//     query: `${searchInput}`,
-//     include_adult: "false",
-//     language: "es-ES",
-//     page: "1",
-//   },
-//   headers: {
-//     accept: "application/json",
-//     Authorization: "Bearer 29a62049dc373ada0ffcf6191b950e4f",
-//   },
-// };
-//https://api.themoviedb.org/3/search/movie?query=fight&include_adult=false&language=es-ES&page=1&api_key=29a62049dc373ada0ffcf6191b950e4f
-//${API_URL}search/movie?query=${searchInput}&include_adult=false&language=es-ES&page=1&${APIKey}
-// https://api.themoviedb.org/3/movie/550?api_key=29a62049dc373ada0ffcf6191b950e4f
-//${API_URL}
 const searchMovie = async (event) => {
   event.preventDefault();
-  //   try {const res = await axios.get(`${API_URL}search/movie?query=${searchInput}&include_adult=false&language=es-ES&${APIKey}`)
   try {
     const search = searchInput.value;
     const res = await axios.get(
-      `${API_URL}search/movie?query=${search}&include_adult=false&language=es-ES&${APIKey}`
+      `${API_URL}search/movie?query=${search}&include_adult=false&language=es-ES&api_key=${APIKey}`
     );
     const movies = res.data.results;
     showMovies(movies);
@@ -58,21 +69,10 @@ const searchMovie = async (event) => {
   } catch (error) {
     console.error(error);
   }
-
-  // .request(options)
-  // .then((res) => console.log(res.data))
-  // .catch((err) => console.error(err));
-
-  // const search = searchInput.value;
-  // const res = await axios.get(`${API_URL}/?name=${search}`);
-  // const characters = res.data.results;
-  //     showMovies(movies);
-  //   } catch (error) {
-  //     console.error(error);
-  //    }
 };
 
 formSearch.addEventListener("submit", searchMovie);
+
 //Imagen -> poster_path
 //Titulo -> title
 //Descripcion -> overview
