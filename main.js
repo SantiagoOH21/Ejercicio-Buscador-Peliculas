e2d8cdedc744f26260542320479086b46686de83
SantiagoOH21 <soh0621@gmail.com>
2025-04-25 01:06:56 +0200

    genre text

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ const formSearch = document.getElementById("form");
 const searchInput = document.getElementById("searchId");
 
 const imageBaseUrl = "https://image.tmdb.org/t/p/";
-const posterSize = "original";
+const posterSize = "w342"; //"poster_sizes":["w92","w154","w185","w342","w500","w780","original"]
 
-const saveMovieGenres = async () => {
+const obtainMovieGenres = async () => {
   try {
     const res = await axios.get(
       `${API_URL}genre/movie/list?language=es&api_key=${APIKey}`
     );
     const movieGenres = res.data.genres;
-    console.log(movieGenres);
     return movieGenres;
   } catch (error) {
     console.error(error);
+    return [];
   }
 };
 
@@ -29,31 +29,45 @@ const changeMovieGenres = (movieGenres, genreIds) => {
   return genreNames.join(", ");
 };
 
-const genreIds = [18, 28, 53];
-
-const obtainGenre = async () => {
-  const movieGenres = await saveMovieGenres();
-  const genresLog = changeMovieGenres(movieGenres, genreIds);
-  console.log("genresLog:", genresLog);
+const obtainGenreText = async (genreIds) => {
+  if (genreIds.length === 0) {
+    return "Sin género establecido";
+  } else {
+    const movieGenres = await obtainMovieGenres();
+    return changeMovieGenres(movieGenres, genreIds);
+  }
 };
 
-obtainGenre();
+const genreText = async (genreIds) => {
+  try {
+    const modifiedGenre = await obtainGenreText(genreIds);
+    return modifiedGenre;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
+};
 
-const showMovies = (movies) => {
+const showMovies = async (movies) => {
   moviesContainer.innerHTML = "";
-  movies.forEach((movie) => {
-    const imageUrl = `${imageBaseUrl}${posterSize}${movie.poster_path}`;
-    moviesContainer.innerHTML += `
-    <div class="card col-lg-3 col-xs-12 col-md-6">
-       <img src="${imageUrl}" alt="${movie.title}">
-       <div class="card-body">
-       <h3 class="card-header">${movie.title}</h3>
-        <h5 class="card-title">Género: ${movie.genre_ids}</h5>
-        <p>SINOPSIS <br>${movie.overview}</p>
-       </div>
-      </div>
-      `;
-  });
+  try {
+    for (const movie of movies) {
+      const modifiedGenre = await genreText(movie.genre_ids);
+      const imageUrl = `${imageBaseUrl}${posterSize}${movie.poster_path}`;
+      moviesContainer.innerHTML += `
+      <div class="card col-lg-3 col-xs-12 col-md-6">
+         <img src="${imageUrl}" alt="${movie.title}">
+         <div class="card-body">
+         <h3 class="card-header">${movie.title}</h3>
+          <h5 class="card-title mt-2">Género: ${modifiedGenre}</h5>
+          <p>SINOPSIS <br>${movie.overview}</p>
+         </div>
+        </div>
+        `;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const searchMovie = async (event) => {
@@ -64,8 +78,7 @@ const searchMovie = async (event) => {
       `${API_URL}search/movie?query=${search}&include_adult=false&language=es-ES&api_key=${APIKey}`
     );
     const movies = res.data.results;
-    showMovies(movies);
-    console.log(res.data.results);
+    await showMovies(movies);
   } catch (error) {
     console.error(error);
   }
